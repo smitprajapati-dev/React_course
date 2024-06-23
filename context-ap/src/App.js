b18e0fec650b8cff3d279a09facae7d4d34259c1
smit prajapati <smitprajapati002@gmail.com>
2024-06-23 07:51:19 -0600

    context-ap basic understand

---
+++ context-ap/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Login from "./Components/Login";
+import Profile from "./Components/Profile";
+import UserContextProvider from "./context/UseContextProvider";
+
+function App() {
+  return (
+    <UserContextProvider>
+      <h1>Smit Prajapati</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  );
+}
+
+export default App;
